7f52573535f90eef3f519a58a5a88b4b8d4de53f
Aaron Kurowski <aaron12342@att.net>
2021-06-04 23:29:34 -0500

    Playing around with overall structure of the application. More styling and component files added.

---
+++ src/components/CollectionStack/collectionStack.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './collectionStack.css';
+
+// put functions in app.jsx and pass them here. just a hook?
+
+const CollectionStack = (props) => {
+    return(
+        <div className="test">
+            Test text
+        </div>
+    );
+}
+
+export default CollectionStack;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import CollectionList from './CollectionList/collectionList.jsx';
 import './app.css';
 import axios from 'axios';
+import CollectionStack from './CollectionStack/collectionStack.jsx';
+import Footer from './Footer/footer.jsx';
+
 
 class App extends Component {
     constructor(props){
@@ -16,26 +19,25 @@ class App extends Component {
         }
     }
 
-    
-
     getCollections = async () => {
         let query = "http://127.0.0.1:8000/collection/";
         let collections = await axios.get(query);
         this.setState({collectionList: collections.data});
-        debugger;
         console.log(this.state.collectionList);
     }
 
     handleSelect = (collection) => {
-        // this.setState({selectedCollection: collection})
+        // after clicking the flashcard div, set the content body with the contents of that collection
     }
 
     render(){
         return(
-            <React.Fragment>
+            <div className="main-container">
                 <CollectionList selectCollection={this.handleSelect} collections={this.state.collectionList}/>
                 <h1>Flashcards Study Tool</h1>
-            </React.Fragment>
+                <CollectionStack />
+                <Footer />
+            </div>
         );
     }
 }
