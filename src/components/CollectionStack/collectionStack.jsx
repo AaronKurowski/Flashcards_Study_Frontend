9aa516d08a3cf209ba51658918258899863ad9c4
Aaron Kurowski <aaron12342@att.net>
2021-06-07 10:31:41 -0500

    Added a flip button to allow user to access the other side of the card. Fixed font size of prompt to be more responsive.

--- src/components/CollectionStack/collectionStack.jsx
+++ src/components/CollectionStack/collectionStack.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './collectionStack.css';
 
 // put functions in app.jsx and pass them here. just a hook?
 
 const CollectionStack = (props) => {
 
-
-
-
-
+    // might need this idk
+    // [currentCard, setCurrentCard] = useState("")
 
     return(
-        <div className="content-container">
-            <div className="previous-btn">
-                <button>Back</button>
-            </div>
-            <div className="top-card">
-                <div className="card-text">
-                    {ReturnCorrectCard(props)}
+        <React.Fragment>
+            <div className="content-container">
+
+                <div className="previous-btn">
+                    <button className="prev-next-btn">Back</button>
                 </div>
-                {/*these will render separately */}
+
+                <div className="top-card">
+                    <div className="card-text">
+                        {ReturnCorrectCard(props)}
+                    </div>
+                </div>
+
+                <div className="next-btn">
+                    <button className="prev-next-btn">Next</button>
+                </div>
+
+                {/*Flip button */}
             </div>
-            <div className="next-btn">
-                <button>Next</button>
+
+            <div className="flip-div">
+                <button onClick className="flip-btn">Flip!</button>
             </div>
-        </div>
+        </React.Fragment>
     );
 }
 
